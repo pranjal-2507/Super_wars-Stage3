de7b887ffc6d55ae1cf9a068ea0f9a5388ba1ca5
pranjal-2507 <pranjalgosavi2507@gmail.com>
2023-10-31 21:24:29 +0530

    Done

---
+++ src/app.js
@@ -0,0 +1,84 @@
+const PLAYERS = [
+    "Spiderman",
+    "Captain America",
+    "Wonderwoman",
+    "Popcorn",
+    "Gemwoman",
+    "Bolt",
+    "Antwoman",
+    "Mask",
+    "Tiger",
+    "Captain",
+    "Catwoman",
+    "Fish",
+    "Hulk",
+    "Ninja",
+    "Black Cat",
+    "Volverine",
+    "Thor",
+    "Slayer",
+    "Vader",
+    "Slingo"
+];
+
+// initialize players with image and strength
+const initPlayers = (players) => {
+    let detailedPlayers = [];
+
+    // Instead of forloop use Map method
+    // Code here
+    players.map(function(players,i){
+        let newplayer = {}
+        newplayer.name = players[i];
+        newplayer.image = `./images/super-${i+1}.png`;
+        newplayer.strength= getRandomStrength();
+        if(i%2==0){
+            newplayer.type = "hero";
+        }
+        else{
+            newplayer.type = "villain"
+        }
+        detailedPlayers.push(newplayer);
+    })
+
+    
+
+    return detailedPlayers;
+}
+
+// getting random strength
+const getRandomStrength = () => {
+    return Math.ceil(Math.random() * 100);
+}
+
+// Build player template
+const buildPlayers = (players, type) => {
+    let fragment = '';
+
+    // Instead of using for loop
+    // Use chaining of Array methods - filter, map and join
+    // Type your code here
+    fragment = players.filter((player) => player.type === type).map((player,i) =>
+     `<div class="player">
+    <img src="${players[i].image}" alt="">
+    <div class="name">${players[i].name}</div>
+    <div class="strength">${players[i].strength}</div>
+    </div>`
+    ).join("")
+    return fragment;
+}
+
+
+    
+
+
+// Display players in HTML
+const viewPlayers = (players) => {
+    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
+    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
+}
+
+
+window.onload = () => {
+    viewPlayers(initPlayers(PLAYERS));
+}
